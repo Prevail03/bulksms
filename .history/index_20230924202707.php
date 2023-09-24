4011285655e6aeb5b2d6d156831ccf052983a899
Prevailer Muhani <71854258+Prevail03@users.noreply.github.com>
2023-09-24 21:35:53 +0300

    initial commit

---
+++ .history/index_20230924202707.php
@@ -0,0 +1,18 @@
+<?php
+
+use AfricasTalking\SDK\AfricasTalking;
+
+$username = 'sandbox'; // use 'sandbox' for development in the test environment
+$apiKey = 'b3aa70ace9f3c5e9458fac4ce13affa4854b810be6f500a866784d01fc74a7d4'; // use your sandbox app API key for development in the test environment
+$AT = new AfricasTalking($username, $apiKey);
+
+// Get one of the services
+$sms = $AT->sms();
+
+// Use the service
+$result = $sms->send([
+    'to' => '+254701694441',
+    'message' => 'Hello World!',
+]);
+
+print_r($result);
---
+++ .history/index_20230924210551.php
@@ -0,0 +1,33 @@
+<?php
+
+require __DIR__.'/vendor/autoload.php';
+use AfricasTalking\SDK\AfricasTalking;
+
+$username = 'sandbox'; // use 'sandbox' for development in the test environment
+$apiKey = 'b3aa70ace9f3c5e9458fac4ce13affa4854b810be6f500a866784d01fc74a7d4'; // use your sandbox app API key for development in the test environment
+$AT = new AfricasTalking($username, $apiKey);
+
+// List of recipients
+$recipients = [
+    '+254701694441', // Recipient 1
+    '+254701694442', // Recipient 2
+    // Add more recipient phone numbers here
+];
+
+// Get the SMS service
+$sms = $AT->sms();
+
+// Send the same message to all recipients
+$result = $sms->send([
+    'to' => $recipients,
+    'message' => 'Heloo World!',
+]);
+
+// Check the result for success or failure
+if ($result['status'] === 'success') {
+    echo "Message sent to all recipients: \n";
+    echo $result;
+} else {
+    echo "Failed to send message to all recipients\n";
+    echo $result;
+}
